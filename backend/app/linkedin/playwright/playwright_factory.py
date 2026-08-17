e8672d9b85b24041af75561cc8fecd3b4925c1d3
VaibhavKaremgar <karemgarvaibhav@gmail.com>
2026-08-17 19:11:39 +0530

    updated the summary

--- backend/app/linkedin/playwright/playwright_factory.py
+++ backend/app/linkedin/playwright/playwright_factory.py
@@ -7,6 +7,7 @@ from typing import Any
 from app.linkedin.playwright.browser_context import BrowserContextConfig
 from app.linkedin.playwright.browser_exceptions import ConfigurationError
 
+
 logger = logging.getLogger(__name__)
 
 
